57d1c371c9fd8180567bbbff7c116a719dc26fcf
Imre Kerr <imre.kerr@knowit.no>
2017-04-28 13:10:45 +0200

    Add posts! :D

--- src/imageboard/index.js
+++ src/imageboard/index.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 
 import PostComponent from './post.js';
+import UploaderComponent from './uploader.js';
 
 class ImageBoardComponent extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: [
+        {imageDataUrl: "http://lorempixel.com/200/150", postText: "FIRST!!1"},
+        {imageDataUrl: "http://lorempixel.com/200/100", postText: "Hei"},
+      ]
+    };
+  }
+  _addPost(post) {
+    this.setState({
+      posts: this.state.posts.concat(post)
+    });
+  }
   render() {
     return (
       <div className="fantastic-component" style={{display: "flex", flexDirection: "column"}}>
-        <PostComponent imageDataUrl="http://lorempixel.com/200/150" postText="FIRST!!1" />
-        <PostComponent imageDataUrl="http://lorempixel.com/200/200" postText="Hei :)s" />
+        <UploaderComponent addPostCallback={(post) => this._addPost(post)} />
+        {this.state.posts.map((post) => <PostComponent imageDataUrl={post.imageDataUrl} postText={post.postText} />)}
       </div>
     );
   }
